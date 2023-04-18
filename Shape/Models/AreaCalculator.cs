ea80a18b96ee519c37a3043a326c2a7c52f04bec
Thomas.C <thomas.caron@viacesi.fr>
2023-04-18 14:49:13 +0200

    feat rectangle class

---
+++ Shape/Models/AreaCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape.Models
+{
+    public class AreaCalculator
+    {
+        public double TotalArea(Rectangle[] arrRectangles)
+        {
+            double area = 0;
+            foreach (Rectangle objRectangle in arrRectangles) 
+            {
+                area += objRectangle.Height * objRectangle.Wight;
+            }
+            return area;
+        }
+    }
+}
